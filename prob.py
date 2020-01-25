c035857d4e551bdd50bf2f89edf69bebb310a8aa
cozenakai <masaki2970@icloud.com>
2020-01-25 23:06:55 +0900

    COMPLETED!!!!!!!Thx

--- prob.py
+++ prob.py
@@ -3,6 +3,10 @@ import random
 from matplotlib import pyplot
 random.random()
 
+theory1= 55/216
+theory2=108/216
+theory3=25/216
+
 collectionA50=[]
 collectionA100=[]
 collectionA1000=[]
@@ -106,7 +110,7 @@ print(collectionC.count(True)/len(collectionC))
 
 
 
-x= [50,100,1000,10000,100000]
+x= ['50','100','1000','10000','100000']
 
 y= [collectionA50.count(True)/50,
     collectionA100.count(True)/100,
@@ -114,17 +118,61 @@ y= [collectionA50.count(True)/50,
     collectionA10000.count(True)/10000,
     collectionA.count(True)/100000
     ]
+y2= [theory1,theory1,theory1,theory1,theory1]
 
+y3=[collectionB50.count(True)/50,
+    collectionB100.count(True)/100,
+    collectionB1000.count(True)/1000,
+    collectionB10000.count(True)/10000,
+    collectionB.count(True)/100000]
 
+y4=[theory2,theory2,theory2,theory2,theory2]
 
-pyplot.scatter(x, y, label="1st experimental")
+y5=[collectionC50.count(True)/50,
+    collectionC100.count(True)/100,
+    collectionC1000.count(True)/1000,
+    collectionC10000.count(True)/10000,
+    collectionC.count(True)/100000]
 
-pyplot.title('Probability')
+y6=[theory3,theory3,theory3,theory3,theory3]
+
+pyplot.plot(x,y)
+
+pyplot.plot(x,y2)
+
+pyplot.title('Problem1')
 pyplot.xlabel('Number of Try')
 pyplot.ylabel('Probability')
 
 
 pyplot.show()
+
+pyplot.plot(x,y3)
+
+pyplot.plot(x,y4)
+
+pyplot.title('Problem2')
+pyplot.xlabel('Number of Try')
+pyplot.ylabel('Probability')
+
+
+pyplot.show()#忘れない
+
+
+pyplot.plot(x,y5)
+
+pyplot.plot(x,y6)
+
+pyplot.title('Problem3')
+pyplot.xlabel('Number of Try')
+pyplot.ylabel('Probability')
+
+
+pyplot.show()#忘れない
+
+
+
+
 
 
 
